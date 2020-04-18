2add74fc97193e14e70b0de77b83c9e5d634d0c5
Graham Christensen <graham@grahamc.com>
2020-04-18 17:54:15 -0400

    Initial dumper

---
+++ dump.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p bash shellcheck -I nixpkgs=channel:nixos-unstable
+
+set -eux
+
+shellcheck "$0"
+
+readonly src_dataset=rpool/backups/nixos.org/haumea/safe/postgres
+readonly working_dataset=rpool/scratch/haumea-load-and-dump
+
+zfs list -t snapshot -H -S createtxg -p -o name "$src_dataset" | head -n1
